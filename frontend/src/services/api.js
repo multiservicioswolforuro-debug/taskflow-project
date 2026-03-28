67c1bb91210493a7c142b32b6ed79cb2b7402cdc
Juan Jose Callahuara <multiservicioswolforuro@gmail.com>
2026-03-28 01:48:40 -0400

    fix api baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskflow-project-j0ir.onrender.com"
+  baseURL: "https://taskflow-project-j0ir.onrender.com/api"
 });
 
 export default API;
